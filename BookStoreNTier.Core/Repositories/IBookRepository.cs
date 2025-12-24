b7f25b896270e86249fada44e26cd73ab9a648d7
sena-efkk <senasultankar@gmail.com>
2025-12-24 17:49:42 +0300

    add ıbookrepositoy

---
+++ BookStoreNTier.Core/Repositories/IBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using BookStoreNTier.Core.Entities;
+
+namespace BookStoreNTier.Core.Repositories;
+
+public interface IBookRepository 
+{
+    Task<Book> GetByIdAsync(int id);
+    Task<IEnumerable<Book>> GetBooksByAuthorAsync(string author);
+    Task AddAsync(Book book);
+
+}
---
+++ BookStoreNTier.Data/Repositories/BookRepository.cs
@@ -0,0 +1,30 @@
+﻿using BookStoreNTier.Core.Entities;
+using BookStoreNTier.Core.Repositories;
+using BookStoreNTier.Data.Context;
+
+namespace BookStoreNTier.Data.Repositories;
+
+public class BookRepository : IBookRepository
+{
+    private readonly AppDbContext _dbContext ;
+
+    public BookRepository(AppDbContext dbContext)
+    {
+        this._dbContext= dbContext;
+        
+    }
+    public async Task<Book> GetByIdAsync(int id)
+    {
+        return await _dbContext.Books.FindAsync(id);
+    }
+
+    public Task<IEnumerable<Book>> GetBooksByAuthorAsync(string author)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(Book book)
+    {
+        throw new NotImplementedException();
+    }
+}
